52735a7358eb253c8b66ca09b0e68206e9cb6ac3
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2022-06-12 21:28:19 +0800

    feat(rps): Add gameplay loop (step 6)

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -5,18 +5,41 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
-        return "It's a draw!"
+        return ["It's a draw!", 0]
     }
     else if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') return "You lose! Paper beats Rock"
-        if (computerSelection === 'scissors') return "You win! Rock beats Scissors"
+        if (computerSelection === 'paper') return ["You lose! Paper beats Rock", -1]
+        if (computerSelection === 'scissors') return ["You win! Rock beats Scissors", 1]
     }
     else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') return "You win! Paper beats Rock"
-        if (computerSelection === 'scissors') return "You lose! Scissors beats Paper"
+        if (computerSelection === 'rock') return ["You win! Paper beats Rock", 1]
+        if (computerSelection === 'scissors') return ["You lose! Scissors beats Paper", -1]
     }
     else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') return "You lose! Rock beats Scissors"
-        if (computerSelection === 'paper') return "You win! Scissors beats Paper"
+        if (computerSelection === 'rock') return ["You lose! Rock beats Scissors", -1]
+        if (computerSelection === 'paper') return ["You win! Scissors beats Paper", 1]
     }
-  }
+    return ["Invalid choice", 0]
+}
+
+function game() {
+    let score = 0
+    for (let i = 0; i < 5; i++) {
+        const playerSelecton = prompt("Choose an action: (rock, paper, scissors)")
+        const computerSelection = computerPlay()
+        const [message, scoreOffset] = playRound(playerSelecton, computerSelection)
+        score += scoreOffset
+        console.log(message)
+    }
+    if (score > 0) {
+        console.log("You win!")
+    }
+    else if (score < 0) {
+        console.log("You lose!")
+    }
+    else {
+        console.log("It's a draw!")
+    }
+}
+
+game()
