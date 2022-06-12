05624d0babae6c708d0ca04a6caf29d1f786528a
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2022-06-12 21:11:55 +0800

    feat(rps): Add function to play one round of rps (step 4 + 5)

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,22 @@
 function computerPlay() {
-    return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)]
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection === computerSelection) {
+        return "It's a draw!"
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') return "You lose! Paper beats Rock"
+        if (computerSelection === 'scissors') return "You win! Rock beats Scissors"
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') return "You win! Paper beats Rock"
+        if (computerSelection === 'scissors') return "You lose! Scissors beats Paper"
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') return "You lose! Rock beats Scissors"
+        if (computerSelection === 'paper') return "You win! Scissors beats Paper"
+    }
+  }
